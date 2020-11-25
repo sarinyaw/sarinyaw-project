a3b5c1d2e9521e9fe858a2767b72ac8bdb15bcce
Sarinya Wongmueng <51124721+sarinyaw@users.noreply.github.com>
2020-11-25 18:16:17 +0900

    edit favicon

--- shortly/components/layout.js
+++ shortly/components/layout.js
@@ -15,7 +15,7 @@ export default function Layout({ children }) {
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" href="/images/favicon-32x32.png" />
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
       <nav className={styles.navbar}>
